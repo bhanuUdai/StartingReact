b357fff1d7ee742fceecc2b5131adcbf163839e5
bhanu <udaibhanu2602@gmail.com>
2022-09-13 16:30:15 +0530

    Styling and using JS in JSX Task-8

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import ExpenseItem from "./components/ExpenseItem";  // importing "ExpenseItem" component from folder"components" and file "ExpenseItem" ,here using "ExpenseItem" as html element
+import ExpenseItem from "./components/ExpenseItem";  
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
-      <p>Hello  world</p>
       <ExpenseItem></ExpenseItem>        
     </div>
   );
 }
 
 export default App;
+
+
+// Note: importing "ExpenseItem" component from folder"components" and file "ExpenseItem" ,here using "ExpenseItem" as html element
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,12 +1,36 @@
+import "./ExpenseItem.css";
+
+const expenseDate=new Date(2020,0,26);
+const expenseName="Car Insurance";
+const expensePrice=500;
+const locationOfExpenditure="Shimla"
+
 function ExpenseItem() {
   return (
-    <div>
-      <h1>Expense Items</h1>
-      <div>Food Rs 10</div>
-      <div>Petrol RS 100</div>
-      <div>Movie Rs 200</div>
+    <div className="expense-item">
+      {/* <h1>Expense Items</h1> */}
+
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseName} </h2>
+        <h2 className="expense-item__description h2">{locationOfExpenditure}</h2>
+        <div className="expense-item__price">Rs {expensePrice}</div>
+      </div>
+    
+      {/* <div className="expense-item__description">
+        <h2>Petrol</h2>
+        <div className="expense-item__price">RS 100</div>
+      </div>
+
+      <div className="expense-item__description">
+        <h2>Movie</h2>
+        <div className="expense-item__price">Rs 200</div>
+      </div> */}
     </div>
   );
 }
 
 export default ExpenseItem;
+
+//Note : There should be only one <div> present to root, you cannot write another <div> in its parallel
+// but write inside this paerent <div>
