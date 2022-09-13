bb4e8fe56d35f5c9dfb5480f81be5e7ec19bc619
bhanu <udaibhanu2602@gmail.com>
2022-09-13 12:49:04 +0530

    Adding Expense Beneath ExpenseItems Task-7

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/ExpenseItem";  // importing "ExpenseItem" component from folder"components" and file "ExpenseItem" ,here using "ExpenseItem" as html element
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
       <p>Hello  world</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>        
     </div>
   );
 }
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,7 +1,12 @@
-function ExpenseItem()
-{
-    return <h1>First Component</h1>
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <div>Food Rs 10</div>
+      <div>Petrol RS 100</div>
+      <div>Movie Rs 200</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
-
