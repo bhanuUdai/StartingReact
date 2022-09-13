350b79a555b5d420131ebe62d6b82620880cbf8a
bhanu <udaibhanu2602@gmail.com>
2022-09-13 23:32:41 +0530

    Calling functional component with parameter Task-9

--- App.js
+++ App.js
@@ -1,9 +1,64 @@
-import ExpenseItem from "./components/ExpenseItem";  
+import ExpenseItem from "./components/ExpenseItem.js";
+
+let items = [
+  {
+    id: 1,
+    title: "Car Insurance",
+    amount: 100,
+    date: new Date(2022, 0, 1),
+    location: "Shimla",
+  },
+  {
+    id: 2,
+    title: "Petrol",
+    amount: 500,
+    date: new Date(2022, 0, 2),
+    location: "Una",
+  },
+  {
+    id: 3,
+    title: "Diesel",
+    amount: 400,
+    date: new Date(2022, 0, 3),
+    location: "Kullu",
+  },
+  {
+    id: 4,
+    title: "LPG",
+    amount: 1000,
+    date: new Date(2022, 0, 4),
+    location: "Mandi",
+  },
+];
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>        
+      <h2>Expense Tracker</h2>
+      <ExpenseItem
+        date={items[0].date}
+        title={items[0].title}
+        location={items[0].location}
+        price={items[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={items[1].date}
+        title={items[1].title}
+        location={items[1].location}
+        price={items[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={items[2].date}
+        title={items[2].title}
+        location={items[2].location}
+        price={items[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={items[3].date}
+        title={items[3].title}
+        location={items[3].location}
+        price={items[3].amount}
+      ></ExpenseItem>
     </div>
   );
 }
@@ -11,4 +66,15 @@ function App() {
 export default App;
 
 
-// Note: importing "ExpenseItem" component from folder"components" and file "ExpenseItem" ,here using "ExpenseItem" as html element
+//Here we are passing data in Expenseitems.js through props(properties)
+//here we are re using same copmonent to do so
+// previously we have only one list of expense , but now we have four list of expenses
+// to print them all we use four  "ExpenseItem" elements
+// here we are putting values dynamically i.e expense items are not build in html but we are trasfering from outside
+
+//here  date={items[0].date}   title={items[0].title}  location={items[0].location}   price={items[0].amount}
+//are basically attributes, these are different from HTML attributes, 
+//here we can make attributes dynamic
+// attribute name can be anything , but variable contain array and name of key in object should be same
+
+
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,36 +1,23 @@
 import "./ExpenseItem.css";
 
-const expenseDate=new Date(2020,0,26);
-const expenseName="Car Insurance";
-const expensePrice=500;
-const locationOfExpenditure="Shimla"
-
-function ExpenseItem() {
+function ExpenseItem(prop) {
   return (
     <div className="expense-item">
-      {/* <h1>Expense Items</h1> */}
-
-      <div>{expenseDate.toISOString()}</div>
+      <div>{prop.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseName} </h2>
-        <h2 className="expense-item__description h2">{locationOfExpenditure}</h2>
-        <div className="expense-item__price">Rs {expensePrice}</div>
+        <h2>{prop.title} </h2>
+        <h2 className="expense-item__description h2">{prop.location}</h2>
+        <div className="expense-item__price">Rs {prop.price}</div>
       </div>
-    
-      {/* <div className="expense-item__description">
-        <h2>Petrol</h2>
-        <div className="expense-item__price">RS 100</div>
-      </div>
-
-      <div className="expense-item__description">
-        <h2>Movie</h2>
-        <div className="expense-item__price">Rs 200</div>
-      </div> */}
     </div>
   );
 }
 
 export default ExpenseItem;
 
-//Note : There should be only one <div> present to root, you cannot write another <div> in its parallel
-// but write inside this paerent <div>
+// here ExpenseItem is a single component returning JSX
+//in react functional parameters are called as prop
+// here we use prop as "prop" but it can be any variable (e.g "data" etc)
+// text content in JSX have {parameter.name of attribute we have used in ExpenseItem element in App.js}
+//Note :
+// .toISOString() returns date as string
